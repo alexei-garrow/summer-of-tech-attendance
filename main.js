a3f496954ac93a6db77db1ae194172e15713f0be
Alexei Garrow <alexei.garrow@gmail.com>
2025-09-18 14:43:36 +1200

    array of objects to store temporary data

---
+++ main.js
@@ -0,0 +1,4 @@
+//Each attendance entry has multiple attributes. An object is perfect for grouping them. An array lets us store multiple records and iterate easily.
+let records = [
+  { student: "Student A", date: "2025-09-18", status: "Present" }
+];
